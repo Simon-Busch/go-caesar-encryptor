50ab164c81111ac881a5a5ccc1ad15273dbd6801
Simon Busch <Simon.busch@live.fr>
2024-07-18 05:28:05 +0200

    wrap up encrypt / decrypt

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const originalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 
 func main() {
 	plainText := "HELLOWORLD"
@@ -11,13 +17,47 @@ func main() {
 
 	decryptedText := decrypt(5, encryptedText)
 	fmt.Println("Decrypted text: ", decryptedText)
-	
+
 }
 
 func encrypt(key int, plainText string) (result string) {
+	hashLetter := hashLetterFn(key, originalLetters)
 
+	var hashedString = ""
+	findOne := func(r rune) rune {
+		pos := strings.Index(originalLetters, string([]rune{r}))
+		if pos != -1 {
+			letterPosition := (pos + len(originalLetters)) % len(originalLetters)
+			hashedString += string(hashLetter[letterPosition])
+		}
+		return r
+	}
+
+	strings.Map(findOne, plainText)
+	return hashedString
 }
 
 func decrypt(key int, encryptedText string) (result string) {
+	hashLetter := hashLetterFn(key, originalLetters)
+	var hashedString = ""
+	findOne := func(r rune)rune {
+		pos := strings.Index(hashLetter, string([]rune{r}))
+		if pos != -1 {
+			letterPosition := (pos + len(originalLetters)) % len(originalLetters)
+			hashedString += string(originalLetters[letterPosition])
+			return r
+		}
+		return r
+	}
 
+
+	strings.Map(findOne, encryptedText)
+	return hashedString
+}
+
+func hashLetterFn(key int, letter string) (result string) {
+	runes := []rune(letter)
+	lastLetterKey := string(runes[len(letter)-key : len(letter)])
+	leftOverLetter := string(runes[0 : len(letter)-key])
+	return fmt.Sprintf("%s%s", lastLetterKey, leftOverLetter)
 }
